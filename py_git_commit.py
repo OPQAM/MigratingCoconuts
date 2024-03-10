9d1ab4835b8958f1721d73ab537196ee5d22ab1f
OPQAM <theopqam@protonmail.com>
2024-03-10 10:16:34 +0000

    this got deleted. Need to consider with more care git commands

---
+++ py_git_commit.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+# Re-adding this file on github. For some reason, it got deleted. Need to figure out why
+
+import subprocess
+selection = ""
+
+while True:
+    choice = input(f"Is this commit a new feature(f), a doc(d), beautification(b), or a test(t)?")
+    if choice == "f":
+        selection = "Feature"
+        break
+    elif choice == "d":
+        selection = "Documentation"
+        break
+    elif choice == "b":
+        selection = "Style"
+        break
+    elif choice == "t":
+        selection = "Test"
+        break
+    else:
+        print("Please make an appropriate choice.")
+
+goal = input("What was the issue that you were trying to address?\n")
+action = input(f"What have you accomplished with this particular {selection} commit?\n")
+
+standard_commit = '''git commit -m "Type: '{}'" -m "Objective: '{}'" -m "Fixes: '{}'"'''.format(selection, goal, action)
+
+subprocess.run(standard_commit, shell=True)
