e1c74a0636f3c287a837224202c0c2ae84033026
OPQAM <theopqam@protonmail.com>
2024-03-10 10:54:24 +0000

    'Feature': simplified the code and made it more in line with github's presentation standards
    Objective: 'to have a working, automated and standardized git-commit script'

--- py_git_commit.py
+++ py_git_commit.py
@@ -3,28 +3,22 @@
 # Re-adding this file on github. For some reason, it got deleted. Need to figure out why
 
 import subprocess
-selection = ""
+
+print('Standardized commit message (substituting commit -m "message_here")\n')
+
+issue = {'f': 'Feature', 'd': 'Doc', 'b': 'Style', 't': 'Test'}
 
 while True:
-    choice = input(f"Is this commit a new feature(f), a doc(d), beautification(b), or a test(t)?")
-    if choice == "f":
-        selection = "Feature"
-        break
-    elif choice == "d":
-        selection = "Documentation"
-        break
-    elif choice == "b":
-        selection = "Style"
-        break
-    elif choice == "t":
-        selection = "Test"
+    choice = input(f"Is this commit a new Feature(f), Documentation(d), Beautification or Style(b), or a Test(t)?")
+    if choice in issue:
+        user_choice = issue[choice]
         break
     else:
         print("Please make an appropriate choice.")
 
-goal = input("What was the issue that you were trying to address?\n")
-action = input(f"What have you accomplished with this particular {selection} commit?\n")
+goal = input(f"What was the aim of this {user_choice} change?\n")
+action = input(f"What has actually been done?\n")
 
-standard_commit = '''git commit -m "Type: '{}'" -m "Objective: '{}'" -m "Fixes: '{}'"'''.format(selection, goal, action)
+standard_commit = '''git commit -m "'{}': {}" -m "Objective: '{}'"'''.format(user_choice, action, goal)
 
 subprocess.run(standard_commit, shell=True)
