7bd6c8476f3dd62ecf877b91fd58c411d4f16fdc
OPQAM <theopqam@protonmail.com>
2024-03-13 19:19:48 +0000

    Style: corrected some typos
    Objective: Minor cleaning of files

--- RandomStuffsesses/simpleReview.py
+++ RandomStuffsesses/simpleReview.py
@@ -8,10 +8,10 @@ counter = input("How many items do you want to append?\n")
 try:
   numericalcounter = int(counter)
 except:
-  sys.exit("That's nota  valid integer") # Using sys.exit here to actually stop the program
+  sys.exit("That's not a valid integer") # Using sys.exit here to actually stop the program
 
 for i in range(numericalcounter):
-  valuetolist = input("Tell me something you'd like to append tot he list: ")
+  valuetolist = input("Tell me something you'd like to append to the list: ")
   lista.append(valuetolist)
 
 print(lista)
