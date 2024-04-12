5c8c83b9f4345c7388a1aa6a81f585f0fb11edb1
OPQAM <theopqam@protonmail.com>
2024-04-12 10:29:33 +0100

    Feature: Turned the pyramid into Sudan
    Objective: Pyramid app

--- ReviewExercises/numberPyramid.py
+++ ReviewExercises/numberPyramid.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 
+# Started as a simple pyramid, then a complete pyramid, then Sudan
+# Different versions in github, 12/04/2024:
+# https://github.com/OPQAM/MigratingCoconuts/tree/master/ReviewExercises
+
 while True:
     pyramidValue = input("Gimme a positive integer: ")
     try:
@@ -11,15 +15,15 @@ while True:
             print(f"'{pyramidValue}' isn't a positive integer.")
     except ValueError:
         print(f"'{pyramidValue}' isn't a valid number.")
-
-counter = 1
-for i in range(pyramidValue):
-    print((str(counter) + ' ') * (i+1))
-    counter += 1
-
-pyramidValue -= 1
-inverseCounter = pyramidValue
-if pyramidValue != 0:
+while pyramidValue != 0:
+    counter = 1
     for i in range(pyramidValue):
-        print((str(inverseCounter) + ' ') * (inverseCounter))
-        inverseCounter -= 1
+        print((str(counter) + ' ') * (i+1))
+        counter += 1
+
+    pyramidValue -= 1
+    inverseCounter = pyramidValue
+    if pyramidValue != 0:
+        for i in range(pyramidValue):
+            print((str(inverseCounter) + ' ') * (inverseCounter))
+            inverseCounter -= 1
