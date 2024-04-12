a6ac32279a4f6c32e8a4e6c65f4f7fa55c582d7b
OPQAM <theopqam@protonmail.com>
2024-04-12 10:25:09 +0100

    Feature: Version 2 - full pyramid
    Objective: pyramid app

--- ReviewExercises/numberPyramid.py
+++ ReviewExercises/numberPyramid.py
@@ -17,3 +17,9 @@ for i in range(pyramidValue):
     print((str(counter) + ' ') * (i+1))
     counter += 1
 
+pyramidValue -= 1
+inverseCounter = pyramidValue
+if pyramidValue != 0:
+    for i in range(pyramidValue):
+        print((str(inverseCounter) + ' ') * (inverseCounter))
+        inverseCounter -= 1
